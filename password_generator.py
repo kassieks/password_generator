410198f85dd01f4f6586f050265e6c308327362b
Hbl9204 <lenchubert@gmail.com>
2022-08-25 11:32:57 +0200

    created body of program

---
+++ password_generator.py
@@ -0,0 +1,8 @@
+import random, string
+pass_len = (input("How long would You have Your passwort :) ? : "))
+all_characters = string.ascii_letters + string.digits + string.punctuation
+password = ""
+for number in range (int(pass_len)):
+    password += random.choice(all_characters) 
+print("Your password is:", password)
+
